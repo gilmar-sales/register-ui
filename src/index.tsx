112abb6e8015d9e609c952e7672bbfe6bceafc7b
gilmarxd <gilmar_custodio_@hotmail.com>
2022-04-30 12:24:35 -0300

    🐛 remove react strict mode that makes the use subscription fail

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
-import { ColorModeScript } from "@chakra-ui/react";
 import * as React from "react";
+import { ColorModeScript } from "@chakra-ui/react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -7,10 +7,10 @@ import * as serviceWorker from "./serviceWorker";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
-  <React.StrictMode>
+  <>
     <ColorModeScript />
     <App />
-  </React.StrictMode>
+  </>
 );
 
 serviceWorker.unregister();
