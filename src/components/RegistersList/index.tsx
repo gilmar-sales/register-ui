c862b296f6d2e74753e6ceb0cc8593d7dde84509
gilmarxd <gilmar_custodio_@hotmail.com>
2022-04-30 12:21:24 -0300

    ♻️ extract registers list

---
+++ src/components/RegistersList/index.tsx
@@ -0,0 +1,90 @@
+import {
+  Box,
+  Flex,
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
+  Text,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
+import React from "react";
+import Register from "../../@types/Register";
+
+interface RegistersListProps {
+  registers: Register[];
+}
+
+const RegistersList: React.FC<RegistersListProps> = ({ registers }) => {
+  return (
+    <TableContainer w="100%" overflow={"auto"}>
+      <Table
+        variant="unstyled"
+        size="lg"
+        style={{
+          borderSpacing: "0 1rem",
+          borderCollapse: "separate",
+        }}
+      >
+        <Thead>
+          <Tr>
+            <Th>Collaborator</Th>
+            <Th>Date</Th>
+            <Th>Hour</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {registers.map((register) => (
+            <Tr key={register.id}>
+              <Td padding="1rem" bg="white" borderLeftRadius="1rem">
+                <Flex alignItems="center">
+                  <Box
+                    width="5px"
+                    height="2rem"
+                    bg={register.type === "in" ? "green.300" : "red.300"}
+                    marginRight="25%"
+                    borderRadius="1rem"
+                  />
+                  <Box>
+                    <Text fontSize="2xl">{register.user.name}</Text>
+                    <Text fontSize="xs">
+                      {Number(register.user.id).toString().padStart(3, "0")}
+                    </Text>
+                  </Box>
+                </Flex>
+              </Td>
+              <Td bg="white">
+                <Text color="blackAlpha.500" fontSize="2xl">
+                  {`${new Date(register.timeRegistered).toLocaleDateString(
+                    "en-US",
+                    {
+                      day: "2-digit",
+                      month: "2-digit",
+                      year: "2-digit",
+                    }
+                  )}`}
+                </Text>
+              </Td>
+              <Td bg="white" borderRightRadius="1rem" isNumeric>
+                <Text color="blackAlpha.600" fontWeight="bold" fontSize="3xl">
+                  {`${new Date(register.timeRegistered).toLocaleString(
+                    "en-US",
+                    {
+                      hour12: false,
+                      hour: "2-digit",
+                      minute: "2-digit",
+                    }
+                  )}h`}
+                </Text>
+              </Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+    </TableContainer>
+  );
+};
+
+export default RegistersList;
--- src/pages/Registers/index.tsx
+++ src/pages/Registers/index.tsx
@@ -1,20 +1,10 @@
+import React, { useEffect, useState } from "react";
 import { gql, useQuery } from "@apollo/client";
-import {
-  Box,
-  Button,
-  Flex,
-  Table,
-  TableContainer,
-  Tbody,
-  Td,
-  Text,
-  Th,
-  Thead,
-  Tr,
-} from "@chakra-ui/react";
-import React from "react";
+import { Box, Button, Flex } from "@chakra-ui/react";
+
 import Register from "../../@types/Register";
 import LogoIcon from "../../components/LogoIcon";
+import RegistersList from "../../components/RegistersList";
 import TabsPanel from "../../components/TabsPanel";
 
 const REGISTERS = gql`
@@ -34,9 +24,16 @@ const REGISTERS = gql`
 `;
 
 const Registers: React.FC = () => {
-  const { data, loading } = useQuery(REGISTERS);
+  const [registers, setRegisters] = useState<Register[]>([]);
 
-  const registers = data.findUserRegisters as Register[];
+  const { data, loading } = useQuery(REGISTERS);
+  const btnRef = React.useRef<HTMLButtonElement>(null);
+
+  useEffect(() => {
+    if (data) {
+      setRegisters(data.findUserRegisters);
+    }
+  }, [data]);
 
   return (
     <Flex>
@@ -62,86 +59,13 @@ const Registers: React.FC = () => {
         >
           <Box width="80%">
             <Flex justifyContent="flex-end" width="100%" marginBottom="3rem">
-              <Button
-                size="lg"
-                colorScheme="green"
-                borderRadius="none"
-                fontWeight="extrabold"
-              >
+              <Button ref={btnRef} size="lg" fontWeight="extrabold">
                 Register
               </Button>
             </Flex>
 
             <Flex justifyContent="center" width="100%">
-              <TableContainer w="100%">
-                <Table
-                  variant="unstyled"
-                  size="lg"
-                  style={{
-                    borderSpacing: "0 1rem",
-                    borderCollapse: "separate",
-                  }}
-                >
-                  <Thead>
-                    <Tr>
-                      <Th>Collaborator</Th>
-                      <Th>Date</Th>
-                      <Th>Hour</Th>
-                    </Tr>
-                  </Thead>
-                  <Tbody>
-                    {registers.map((register) => (
-                      <Tr>
-                        <Td padding="1rem" bg="white" borderLeftRadius="1rem">
-                          <Flex alignItems="center">
-                            <Box
-                              width="5px"
-                              height="2rem"
-                              bg="green.300"
-                              marginRight="25%"
-                              borderRadius="1rem"
-                            />
-                            <Box>
-                              <Text fontSize="2xl">{register.user.name}</Text>
-                              <Text fontSize="xs">
-                                {Number(register.user.id)
-                                  .toString()
-                                  .padStart(3, "0")}
-                              </Text>
-                            </Box>
-                          </Flex>
-                        </Td>
-                        <Td bg="white">
-                          <Text color="blackAlpha.500" fontSize="2xl">
-                            {`${new Date(
-                              register.timeRegistered
-                            ).toLocaleDateString("en-US", {
-                              day: "2-digit",
-                              month: "2-digit",
-                              year: "2-digit",
-                            })}`}
-                          </Text>
-                        </Td>
-                        <Td bg="white" borderRightRadius="1rem" isNumeric>
-                          <Text
-                            color="blackAlpha.600"
-                            fontWeight="bold"
-                            fontSize="3xl"
-                          >
-                            {`${new Date(
-                              register.timeRegistered
-                            ).toLocaleString("en-US", {
-                              hour12: false,
-                              hour: "2-digit",
-                              minute: "2-digit",
-                            })}h`}
-                          </Text>
-                        </Td>
-                      </Tr>
-                    ))}
-                  </Tbody>
-                </Table>
-              </TableContainer>
+              <RegistersList registers={registers} />
             </Flex>
           </Box>
         </Flex>
