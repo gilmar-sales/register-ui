ac7adf4de17b7ab58558d3409d1e7cc343cf1c8e
gilmarxd <gilmar_custodio_@hotmail.com>
2022-04-29 23:13:28 -0300

    ✨ add nunito font

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
 import * as React from "react";
-import { ChakraProvider, CSSReset, theme } from "@chakra-ui/react";
+import { ChakraProvider, CSSReset, extendTheme } from "@chakra-ui/react";
 import Routing from "./Routing";
 import { ApolloProvider } from "@apollo/client";
 import api from "./services/api";
 import { AuthContextProvider } from "./contexts/AuthContext";
-import { UserContextProvider } from "./contexts/UserContext";
+
+const theme = extendTheme({
+  fonts: {
+    body: "Nunito",
+    heading: "Nunito",
+    mono: "Nunito",
+  },
+});
 
 export const App = () => (
   <ChakraProvider theme={theme}>
     <ApolloProvider client={api}>
       <AuthContextProvider>
-        <UserContextProvider>
-          <Routing />
-          <CSSReset />
-        </UserContextProvider>
+        <Routing />
+        <CSSReset />
       </AuthContextProvider>
     </ApolloProvider>
   </ChakraProvider>
